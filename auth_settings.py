a0e2455f499aa29aa77963770005df331eae504c
Elijah Baraza <ebaraza10@gmail.com>
2019-10-05 17:22:24 +0300

    Added auth2 support for django rest framework

---
+++ auth_settings.py
@@ -0,0 +1,12 @@
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'oauth2_provider.contrib.rest_framework.OAuth2Authentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    )
+}
+OAUTH2_PROVIDER = {
+    # this is the list of available scopes
+    'SCOPES': {'read': 'Read scope', 'write': 'Write scope', 'groups': 'Access to your groups'}
+}
